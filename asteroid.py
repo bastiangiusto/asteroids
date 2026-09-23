43a692b0c8b046fee85b8bfe53555406edfb91b6
bastiangiusto <giustobastian@gmail.com>
2026-09-23 07:19:00 +0200

    feature(asteroid, main): added the collides_with method to main to check for collisions with the player. correct an implementation detail in asteroid.

--- asteroid.py
+++ asteroid.py
@@ -17,4 +17,4 @@ class Asteroid(CircleShape):
         )
 
     def update(self, dt: float) -> None:
-        self.position = self.velocity * dt
+        self.position += self.velocity * dt
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
+import sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
@@ -31,7 +32,7 @@ def main():
     asteroids = pygame.sprite.Group()
 
     Asteroid.containers = (asteroids, updatable, drawable)
-    AsteroidField.containers = (updatable)
+    AsteroidField.containers = (updatable,)
 
     # Spawning a Asterofield object
     asterofield = AsteroidField()
@@ -48,13 +49,19 @@ def main():
         screen.fill("black")
         
         # Render the player on the screen before flipping the screen
-        for player in drawable:
-            player.draw(screen)
+        for drawable_obj in drawable:
+            drawable_obj.draw(screen)
 
 
         # Rotate player if key is pressed
         updatable.update(dt)
 
+        # Iterating over all asteroids objects to check for collision
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
         
         # Updating the screen
         pygame.display.flip()
@@ -62,8 +69,6 @@ def main():
 
         # Updating dt
         dt = clock.tick(60) / 1000
-
-
 
 
 if __name__ == "__main__":
